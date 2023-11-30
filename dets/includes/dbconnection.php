221546089349ee191b122cc0e78b51527a7eee9a
msy061618 <94689977+msy061618@users.noreply.github.com>
2023-11-30 19:20:40 +0530

    Update dbconnection.php

--- dets/includes/dbconnection.php
+++ dets/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("192.168.1.17", "root", "12345678", "demodb");
+$con=mysqli_connect("192.168.1.13", "root", "12345678", "demodb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
